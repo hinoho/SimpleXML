125916514a0c2aa643d78b91eb221840e9bb8126
hinoho <31796424+hinoho@users.noreply.github.com>
2018-09-07 21:14:15 +0300

    Delete Main.java

--- src/Main.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-public class Main extends SimpleXMLImpl{
-    public static void main(String[] args) {
-        SimpleXML simpleXML = new SimpleXMLImpl();
-        System.out.println(simpleXML.createXML("tag", "taggy"));
-    }
-
-}
